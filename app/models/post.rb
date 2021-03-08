b9f4b9c875d5075ef0c80cff1b209370de4d8af9
EC2 Default User <ec2-user@ip-172-31-41-210.ap-northeast-1.compute.internal>
2021-03-08 13:54:55 +0000

    機能少しのこってる！

---
+++ app/models/post.rb
@@ -0,0 +1,11 @@
+class Post < ApplicationRecord
+  attachment :image
+  belongs_to :user
+
+  with_options presence: true do
+    validates :title
+    validates :body
+    validates :image
+  end
+
+end
